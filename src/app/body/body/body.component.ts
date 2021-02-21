2787a512b378bcfaec84c5b15863f5dcf475431f
Caesar Feng <caesar@rcafinnovation.ca>
2021-02-20 23:21:32 -0500

    reformatting body section with bootstrap

--- src/app/body/body/body.component.ts
+++ src/app/body/body/body.component.ts
@@ -8,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class BodyComponent implements OnInit {
   rcafLogo: string;
   safutureLogo: string;
+  me : string;
   constructor() { 
     this.rcafLogo = "/assets/company_logos/RCAF.jpg";
     this.safutureLogo = "/assets/company_logos/safuture.png";
+    this.me = "/assets/company_logos/me.jpg";
   }
 
   ngOnInit(): void {
